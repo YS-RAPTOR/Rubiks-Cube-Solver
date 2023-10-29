c11e271ba7c1cbbecd1ac8a96371bb3721ef91fd
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2023-10-29 17:22:25 +1100

    Image processing working nicely

--- main.m
+++ main.m
@@ -1,201 +1,281 @@
 % Relationships between the center and other colors
-% Top, Right, Bottom, Left, Back
+% {Possible colors immediately next to it, Opposite color}
 relationships = containers.Map;
-relationships('w') = 'rbogy';
-relationships('r') = 'ybwgo';
-relationships('o') = 'wbygr';
-relationships('b') = 'ryowg';
-relationships('g') = 'rwoyb';
-relationships('y') = 'rgobw';
+relationships('w') = {'rbog', 'y'};
+relationships('r') = {'ybwg', 'o'};
+relationships('o') = {'wbyg', 'r'};
+relationships('b') = {'ryow', 'g'};
+relationships('g') = {'rwoy', 'b'};
+relationships('y') = {'rgob', 'w'};
 
-% Color bounds
-colorBounds = containers.Map;
-colorBounds('w') = [0, 0, 0.6, 1, 1, 1];
-colorBounds('r') = [0.95, 0.4167, 0.1389, 0.0556, 1, 1];
-colorBounds('o') = [0.0556, 0.4167, 0.1389, 0.1389, 1, 1];
-colorBounds('b') = [0.5556, 0.4167, 0.1389, 0.7222, 1, 1];
-colorBounds('g') = [0.2778, 0.4167, 0.1389, 0.4444, 1, 1];
-colorBounds('y') = [0.1389, 0.4167, 0.1389, 0.1944, 1, 1];
+% Modes:
+% Preview Mode: Click Enter to capture image and go to prompt mode
+% Prompt Mode: Show image captured and prompt user for confirmation of image with y or n. If yes go to next mode else go to preview mode
+% Action Mode: Show User action to take and let them click enter once they finish the action. Then go to preview mode
 
-% Colors
-colors = ['w', 'r', 'o', 'b', 'g', 'y'];
-
-cube = zeros(3,3,6);
-notDetected = 'wrobgy';
-img1 = iread('file1.jpg', 'double');
-DetectRubiksCube(cube, img1, 3)
-
-% The code in this project is based on the above color relationships
-% Detect the orientation of each face using the above relationships
-
-disp(detectOrientation('w', 'g', 'r', relationships))
+% Turn off warnings
+warning('off', 'MATLAB:polyshape:repairedBySimplify');
 
 
-% Given the colors of the top, left, and right faces, detect the orientation
-% Positive rotations are anticlockwise, negative rotations are clockwise
-function rotations = detectOrientation(left, top, right, relationships)
-arguments
-    left char
-    top char
-    right char
-    relationships containers.Map
-end
-rightRotations = 0;
-leftRotations = 0;
-if left == 'r'
-    rightRotations = -1;
-end
-if left == 'o'
-    rightRotations = 1;
-end
 
-leftRel = relationships(left);
-for i = 1:4
-    next = mod(i + 1,5);
-    if next == 0
-        next = 1;
+
+camera = webcam(1);
+camera.Resolution = '640x480';
+figure;
+while true
+% Get image from camera
+    img = im2double(snapshot(camera));
+
+    % Get bounding boxes of facelets
+    [box, noOfFaces, corners] = DetectFaceletBoundingBoxes(img);
+    
+    if (noOfFaces > 5)
+        % Get facelets from bounding boxes
+        box = PredictFacelets(box, corners);
+        noOfFaces = 9;
     end
-    if leftRel(i) == top && leftRel(next) == right
-        leftRotations = i - 1;
+    
+    for i = 1:noOfFaces
+        img = insertShape(img, 'Rectangle', box(i,:), 'LineWidth', 3);
     end
+    imshow(img);
+
 end
-
-rotations = [leftRotations rightRotations];
-end
+clear camera;
 
 
-function [newLeft, newTop, newRight] = ApplyOrientation(left, top, right, rotation)
+
+function [faceletsBox, noOfFacesDetected, corners] = DetectFaceletBoundingBoxes(img, sigma, threshold, aspectRange, centerThreshold)
 arguments
-    left (3,3) chars
-    top (3,3) chars
-    right (3,3) chars
-    rotation (1,2) int8
-end
-
-newLeft = rot90(left, rotation(1));
-newTop = rot90(top, rotation(1));
-newRight = rot90(right, rotation(1));
-newLeft = rot90(newLeft, rotation(2));
-
-if rotation(2) == 1
-    newRight = rot90(newRight, -1);
-    newLeft = rot90(newLeft, 2);
-elseif rotation(2) == -1
-    newRight = rot90(newRight, 1);
-    newTop = rot90(newTop, 2);
-end
-end
-
-function newCube = DetectRubiksCube(cube, img, sigma)
-arguments
-    cube (3,3,6) uint8
     img (:,:,3) double
-    sigma
+    sigma double = 3
+    threshold double = 0.1
+    aspectRange double = 0.2
+    centerThreshold uint8 = 50
 end
-% Detect the edges using DOG
 GGu = kdgauss(sigma);
 
-Iu = iconvolve( img, GGu);
-Iv = iconvolve( img, GGu');
-magDoG = sqrt( Iu.^2 + Iv.^2 );
+% Convolving with the gaussian kernel all 3 channels
+rIu = conv2(img(:,:,1), GGu, 'same');
+rIv = conv2(img(:,:,1), GGu', 'same');
 
-% Binarize the images
-edges = rgb2gray(magDoG);
-edges = imbinarize(edges, 0.1);
+gIu = conv2(img(:,:,2), GGu, 'same');
+gIv = conv2(img(:,:,2), GGu', 'same');
 
-figure;
-imshow(magDoG);
+bIu = conv2(img(:,:,3), GGu, 'same');
+bIv = conv2(img(:,:,3), GGu', 'same');
 
-% Get the contours of the image
-contours = bwboundaries(edges, 8, 'noholes', 'xy');
+binR = sqrt(rIu.^2 + rIv.^2) > threshold;
+binG = sqrt(gIu.^2 + gIv.^2) > threshold;
+binB = sqrt(bIu.^2 + bIv.^2) > threshold;
 
-% Get the lines in the Image
-% Cell of arrays of lines in the image
-% Each line is an array of 4 elements [x1, y1, x2, y2]
-% { [x1, y1, x2, y2 ; x1, y1, x2, y2 ; x1, y1, x2, y2], [x1, y1, x2, y2]}
-parallelLines = {[0 0 0 100]};
-figure;
-imshow(img);
+bin = binR | binG | binB;
+bin = imclose(bin, strel("square", 3));
+bin = imopen(bin, strel("square", 3));
 
-for i = 1:length(contours)
-    if numel(contours{i}) < 10
-        continue;
-    end
-    shape = polyshape(contours{i}(:,2), contours{i}(:,1), 'SolidBoundaryOrientation','ccw');
+% Get the contours from the binary image
+contours = bwboundaries(bin);
+shapes = polyshape.empty(0, length(contours));
+shapeNo = 0;
+% Prune based on aspect ratio and area
+for c = 1:length(contours)
+    shape = polyshape(contours{c}(:,2), contours{c}(:,1));
+    shapeArea = shape.area;
     
-    if shape.NumRegions ~= 1
-        continue;
-    end
-    
-    if area(shape) < 150
-        continue;
-    end
-    
-    points = reducepoly(shape.Vertices, 0.01);
-    
-    if length(points) < 3
+    if shapeArea < 800
         continue
     end
     
-    lines = [points circshift(points,1)]';
-    for l = lines
-        l = l';
-        
-        
-        lLength = sqrt((l(3) - l(1))^2 + (l(4)-l(2))^2);
-        if lLength < 50
-            continue;
+    [xlim,ylim] = shape.boundingbox;
+    
+    aspectRatio = (xlim(1) - xlim(2))/(ylim(1) - ylim(2));
+    
+    if ~(aspectRatio > 1-aspectRange && aspectRatio < 1+aspectRange)
+        continue
+    end
+    
+    boundingArea = (xlim(1) - xlim(2))*(ylim(1) - ylim(2));
+    areaRatio = boundingArea/shapeArea;
+    if areaRatio > 1.5 && areaRatio < 5
+        continue
+    end
+    
+    shapeNo = shapeNo + 1;
+    shapes(shapeNo) = shape;
+end
+
+% If two shapes have similar centers, remove the larger one
+
+smallestShapes = zeros(shapeNo, 1);
+
+for i=1:shapeNo
+    [iuc, ivc] = centroid(shapes(i));
+    similarShapes = false;
+    for j=1:shapeNo
+        if i == j
+            continue
         end
+        [juc, jvc] = centroid(shapes(j));
         
-        for pl = 1:length(parallelLines)
-            if isParallel(l, parallelLines{pl}(1,:))
-                parallelLines{pl} = [parallelLines{pl}; l];
-                break;
-            elseif pl == length(parallelLines)
-                parallelLines{end + 1} = [l];
+        if abs(iuc - juc) < centerThreshold && abs(ivc - jvc) < centerThreshold
+            similarShapes = true;
+            if shapes(i).area < shapes(j).area
+                smallestShapes(i) = 1;
+                break
+            end
+        end
+    end
+    if ~similarShapes
+        smallestShapes(i) = 1;
+    end
+end
+
+% Check if the centers can be confirmed bu other facelets using the fact that they should align
+
+prunedShapes = shapes(smallestShapes == 1);
+faceletsBox = zeros(length(prunedShapes), 4);
+noOfFacesDetected = 0;
+corners = [Inf, Inf, -Inf, -Inf];
+
+for i = 1:length(prunedShapes)
+    [iuc, ivc] = centroid(prunedShapes(i));
+    confirmed = 0;
+    for j = 1:length(prunedShapes)
+        [juc, jvc] = centroid(prunedShapes(j));
+        if abs(iuc - juc) < centerThreshold  || abs(ivc - jvc) < centerThreshold
+            confirmed = confirmed + 1;
+            if confirmed == 2
+                noOfFacesDetected = noOfFacesDetected + 1;
+                [xlim, ylim] = prunedShapes(i).boundingbox;
+                faceletsBox(noOfFacesDetected, :) = [xlim(1), ylim(1), xlim(2) - xlim(1), ylim(2) - ylim(1)];
+                corners(1) = min(corners(1), xlim(1));
+                corners(2) = min(corners(2), ylim(1));
+                corners(3) = max(corners(3), xlim(2));
+                corners(4) = max(corners(4), ylim(2));
+                break
             end
         end
     end
 end
-hold on;
-% Go through each of the parallel lines and remove the lines that are too small
-cols = 1:length(parallelLines);
-cols = squeeze(label2rgb(cols));
-
-for li = 1:length(parallelLines)
-    l = parallelLines{li};
-    for i = 1:size(l,1)
-        line(l(i, [1,3]), l(i, [2,4]), 'Color', cols(li,:));
-    end
-    drawnow;
-end
-hold off;
-
-
-% What is known about the rubiks cube.
-% It is a cube like structure and 3 faces are visible since the expectation is that we are seing a corner.
-% All the squares seen in a face are aligned in a grid. Can use this to determine the squares in a face.
-% All the faces have 9 squares in a 3x3 grid. 3 faces join to create a corner of a cube.
-% All shapes should approximate to a parallelogram.
-
 end
 
-function yes = isParallel(line1, line2, threshold)
+% Faces are sorted int the order below
+% 1 2 3
+% 4 5 6
+% 7 8 9
+function facelets = PredictFacelets(boundingBoxes, corners)
 arguments
-    line1 (1,4) double
-    line2 (1,4) double
-    threshold double = 0.2
+    boundingBoxes (:,4) double
+    corners (1,4) double
 end
-% Check if two lines are parallel
-% Check if the slopes are the same
-slope1 = (line1(4) - line1(2)) / (line1(3) - line1(1));
-slope2 = (line2(4) - line2(2)) / (line2(3) - line2(1));
-if abs(slope1 - slope2) < threshold
-    yes = true;
-    return;
+facelets = zeros(9, 4);
+totalPerimeter = 0;
+
+for i = 1:size(boundingBoxes, 1)
+    totalPerimeter = totalPerimeter + 2 * (boundingBoxes(i, 3) + boundingBoxes(i, 4));
 end
-yes = false;
+
+averageLength = totalPerimeter / (size(boundingBoxes, 1) * 4);
+centerDistance = averageLength * 1.4;
+
+for i = 1:3
+    for j=1:3
+        index = (i-1)*3 + j;
+        topLeft = [corners(1) + (j-1)*centerDistance, corners(2) + (i-1)*centerDistance];
+        found = false;
+        for k = 1:size(boundingBoxes, 1)
+            if boundingBoxes(k,1) - topLeft(1) < averageLength && boundingBoxes(k,2) - topLeft(2) < averageLength && boundingBoxes(k,1) - topLeft(1) > -0.1 && boundingBoxes(k,2) - topLeft(2) > -0.1
+                facelets(index, :) = boundingBoxes(k, :);
+                found = true;
+                break
+            end
+        end
+        if ~found
+            facelets(index, :) = [topLeft(1), topLeft(2), averageLength, averageLength];
+        end
+    end
 end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
